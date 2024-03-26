935017ea09b4a5cf276b027367f3513d2d72a522
Ufuk Bahar <156933518+ThmBachelorApp@users.noreply.github.com>
2024-03-26 01:06:20 +0100

    Update SmartshopApplication.java

--- src/main/java/com/smartshop/SmartshopApplication.java
+++ src/main/java/com/smartshop/SmartshopApplication.java
@@ -9,4 +9,9 @@ public class SmartshopApplication {
     public static void main(String[] args) {
         SpringApplication.run(SmartshopApplication.class, args);
     }
+    
+    // Hinzugefügte Methode, die wahrscheinlich von SonarQube bemängelt wird
+    private static void unusedMethod() {
+        System.out.println("Diese Methode wird nirgends aufgerufen.");
+    }
 }
