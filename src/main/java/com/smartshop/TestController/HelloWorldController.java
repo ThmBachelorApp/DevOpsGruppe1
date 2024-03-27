7fe38dc00169ec19cb121c9be409eefb254c2b1c
Ufuk Bahar <156933518+ThmBachelorApp@users.noreply.github.com>
2024-03-27 15:18:38 +0100

    Update HelloWorldController.java

--- src/main/java/com/smartshop/TestController/HelloWorldController.java
+++ src/main/java/com/smartshop/TestController/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.TesController;
+package com.testcontroller; // Korrigierter Paketname
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
