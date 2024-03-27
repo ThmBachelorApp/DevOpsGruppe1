1eeeb8d0edfc19e0fe8d958347399e8b93c0af01
Ufuk Bahar <156933518+ThmBachelorApp@users.noreply.github.com>
2024-03-27 14:44:15 +0100

    Update HelloWorld.java

--- src/main/java/app/HelloWorld.java
+++ src/main/java/app/HelloWorld.java
@@ -1,1 +1,9 @@
-public class HelloWorld { public static void main(String[] args) { System.out.println("Hello, World 19.03 17:49 Uhr !"); } }
+// Dateipfad: src/main/java/com/example/app/HelloWorld.java
+
+package com.example.app; // Paketdeklaration hinzufügen
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
