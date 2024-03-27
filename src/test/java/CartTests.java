82b68a229522d26478873a127f5d4e091c2baf6a
Ufuk Bahar <156933518+ThmBachelorApp@users.noreply.github.com>
2024-03-27 17:09:40 +0100

    Update CartTests.java

--- src/test/java/CartTests.java
+++ src/test/java/CartTests.java
@@ -1,11 +1,10 @@
-package com.smartshop.service;
-
 import com.smartshop.model.Product;
+import com.smartshop.service.Cart; // Stellen Sie sicher, dass Sie Cart importieren
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CartTests {
+class CartTests { // 'public' wurde entfernt; in JUnit 5 optional
     private Cart cart;
 
     @BeforeEach
@@ -14,14 +13,14 @@ public class CartTests {
     }
 
     @Test
+    void testAddProductAndTotalPrice() {
+        Product product1 = new Product("1", "Java Book", 29.99);
+        Product product2 = new Product("2", "Spring Book", 39.99);
 
-void testAddProductAndTotalPrice() {
-    Cart cart = new Cart();
-    Product product1 = new Product("1", "Java Book", 29.99);
-    Product product2 = new Product("2", "Spring Book", 39.99);
+        cart.addProduct(product1);
+        cart.addProduct(product2);
 
-    cart.addProduct(product1);
-    cart.addProduct(product2);
-
-    assertEquals(69.98, cart.getTotalPrice(), 0.01, "Die Gesamtsumme sollte 69.98 betragen.");
-}}
+        assertEquals(2, cart.getProductCount(), "Der Warenkorb sollte 2 Produkte enthalten.");
+        assertEquals(69.98, cart.getTotalPrice(), 0.01, "Die Gesamtsumme sollte 69.98 betragen.");
+    }
+}
